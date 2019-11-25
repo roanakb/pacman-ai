8d5ce9e70cb40e028ed2db76e510ceb5e8c708c3
shefaliqamar <sqamar@ucsc.edu>
2019-11-25 15:50:50 -0800

    added improvements to defensive agent -- 34/50 wins, 1/50 ties

--- pacai/student/myTeam.py
+++ pacai/student/myTeam.py
@@ -1,7 +1,7 @@
-from pacai.util import reflection
-from pacai.agents.capture.capture import CaptureAgent
-from pacai.agents.capture.reflex import ReflexCaptureAgent
-from pacai.util import counter
+# from pacai.util import reflection
+# from pacai.agents.capture.capture import CaptureAgent
+# from pacai.agents.capture.reflex import ReflexCaptureAgent
+# from pacai.util import counter
 from pacai.agents.capture.reflex import ReflexCaptureAgent
 from pacai.core.directions import Directions
 from pacai.util import counter
@@ -77,6 +77,9 @@ class DefensiveReflexAgent(ReflexCaptureAgent):
         if (myState.isPacman()):
             features['onDefense'] = 0
 
+        features['successorScore'] = 0
+        features['distanceToFood'] = 0
+
         # Computes distance to invaders we can see.
         enemies = [successor.getAgentState(i) for i in self.getOpponents(successor)]
         invaders = [a for a in enemies if a.isPacman() and a.getPosition() is not None]
@@ -85,6 +88,14 @@ class DefensiveReflexAgent(ReflexCaptureAgent):
         if (len(invaders) > 0):
             dists = [self.getMazeDistance(myPos, a.getPosition()) for a in invaders]
             features['invaderDistance'] = min(dists)
+        else:
+            features['onDefense'] = 0
+            myPos = successor.getAgentState(self.index).getPosition()
+            foodList = self.getFood(successor).asList()
+            minDistance = min([self.getMazeDistance(myPos, food) for food in foodList])
+            features['distanceToFood'] = minDistance
+            features['successorScore'] = self.getScore(successor)
+            features['invaderDistance'] = 0
 
         if (action == Directions.STOP):
             features['stop'] = 1
@@ -101,5 +112,7 @@ class DefensiveReflexAgent(ReflexCaptureAgent):
             'onDefense': 100,
             'invaderDistance': -10,
             'stop': -100,
-            'reverse': -2
+            'reverse': -2,
+            'distanceToFood': -1,
+            'successorScore': 102
         }
