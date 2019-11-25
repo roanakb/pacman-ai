1647e8dde4f2e3395708acb4d6f960a4e3197c6b
Roanak Baviskar <roanakbaviskar@gmail.com>
2019-11-24 16:29:58 -0800

    began building offensive agent

--- pacai/student/myTeam.py
+++ pacai/student/myTeam.py
@@ -1,4 +1,5 @@
 from pacai.util import reflection
+from pacai.agents.capture.capture import CaptureAgent
 
 def createTeam(firstIndex, secondIndex, isRed,
         first = 'pacai.agents.capture.dummy.DummyAgent',
@@ -17,3 +18,5 @@ def createTeam(firstIndex, secondIndex, isRed,
         firstAgent(firstIndex),
         secondAgent(secondIndex),
     ]
+
+class OffensiveAgent(CaptureAgent):
