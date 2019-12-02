4f7ef0ba73ad1f4e84de8163e204949048cc5e1a
shefaliqamar <sqamar@ucsc.edu>
2019-12-01 16:40:55 -0800

    Update myTeam.py

--- pacai/student/myTeam.py
+++ pacai/student/myTeam.py
@@ -103,8 +103,6 @@ class DefensiveReflexAgent(ReflexCaptureAgent):
             dists = [self.getMazeDistance(myPos, a.getPosition()) for a in invaders]
             features['invaderDistance'] = min(dists)
             features['onDefense'] = 1
-            features['successorScore'] = 0
-            features['distanceToFood'] = 0
         else:
             features['onDefense'] = 0
             myPos = successor.getAgentState(self.index).getPosition()
